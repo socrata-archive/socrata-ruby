1ac44b3fbed7e30027497c2860b393d2a43aa717
Aiden Scandella <aiden.scandella@socrata.com>
2010-02-05 16:17:42 -0800

    Noop: Added Apache license notice

--- bin/twitter_set.rb
+++ bin/twitter_set.rb
@@ -18,15 +18,26 @@
 #   what to search twitter for
 #
 
+# Copyright (c) 2010 Socrata.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 require 'getoptlong'
 require 'rdoc/usage'
 require 'tempfile'
-
 require 'rubygems'
-
 require 'socrata'
-
 require 'twitter'
 require 'curb'
 
--- lib/socrata/dataset.rb
+++ lib/socrata/dataset.rb
@@ -1,3 +1,17 @@
+# Copyright (c) 2010 Socrata.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 module Socrata
   class Dataset < SocrataAPI
 
@@ -30,7 +44,7 @@ module Socrata
     # Turns a Ruby date object into a string that the API will recognize
     def parse_date(date)
       d = DateTime.strptime(date, @config['date']['format'])
-      return d.strftime(@config['date']['output']) + " GMT" #+ d.zone()
+      return d.strftime(@config['date']['output']) + " GMT"
     end
 
     # Adds a row, immediately posting result to the API server.
